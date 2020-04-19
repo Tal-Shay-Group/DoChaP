885d5ff0c24d8ca1cbc415450775530928f9a8aa
ShaniGalOz <galozs@post.bgu.ac.il>
2020-04-19 12:29:51 +0300

    Added ensemblDomains Builder begining

---
+++ Tool_code/OOP_project/BioMart.ensembl.domains.M_musculus.pfam.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+wget -O C:\Users\galozs\OneDrive\PhD\Projects\DoChaP\DoChaP\Tool_code\OOP_project/data/M_musculus/ensembl/BioMart/M_musculus.Domains.pfam.txt 'http://www.ensembl.org/biomart/martservice?query=<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE Query>
+<Query  virtualSchemaName = "default" formatter = "TSV" header = "1" uniqueRows = "1" count = "" datasetConfigVersion = "0.6" >
+
+  <Dataset name = "mmusculus_gene_ensembl_gene_ensembl" interface = "default" >
+    <Attribute name = "ensembl_transcript_id_version" />
+    <Attribute name = "ensembl_peptide_id_version" />
+    <Attribute name = "interpro" />
+    <Attribute name = "interpro_short_description" />
+    <Attribute name = "interpro_description" />
+    <Attribute name = "interpro_start" />
+    <Attribute name = "interpro_end" />
+    <Attribute name = "pfam" />
+    <Attribute name = "pfam_start" />
+    <Attribute name = "pfam_end" />
+  </Dataset>
+</Query>'
---
+++ Tool_code/OOP_project/BioMart.ensembl.domains.template2.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+wget -O output.txt 'http://www.ensembl.org/biomart/martservice?query=<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE Query>
+<Query  virtualSchemaName = "default" formatter = "TSV" header = "1" uniqueRows = "1" count = "" datasetConfigVersion = "0.6" >
+
+  <Dataset name = "MainSpecies_gene_ensembl" interface = "default" >
+    <Attribute name = "ensembl_transcript_id_version" />
+    <Attribute name = "ensembl_peptide_id_version" />
+    <Attribute name = "interpro" />
+    <Attribute name = "interpro_short_description" />
+    <Attribute name = "interpro_description" />
+    <Attribute name = "interpro_start" />
+    <Attribute name = "interpro_end" />
+    <Attribute name = "extDB" />
+    <Attribute name = "extDB_start" />
+    <Attribute name = "extDB_end" />
+  </Dataset>
+</Query>'
