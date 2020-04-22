44ab2b6e764821bd04521877da95beb6ee4536af
ShaniGalOz <galozs@post.bgu.ac.il>
2020-04-22 15:29:15 +0300

    All species ensembl downloader

---
+++ Tool_code/OOP_project/EnsemblDomainsForAll.py
@@ -0,0 +1,8 @@
+sys.path.append(os.getcwd())
+from DomainsEnsemblBuilder import *
+
+species = ['M_musculus', 'H_sapiens', 'R_norvegicus', 'D_rerio', 'X_tropicalis']
+for sp in species:
+    ens = DomainsEnsemblBuilder(sp, download=True)
+    ens.downloader()
+    print("finish download for {}".format(sp))
