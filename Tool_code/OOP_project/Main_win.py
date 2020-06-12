e3e68c542fc64905d8a34cf58c62924057583afa
ShaniGalOz <galozs@post.bgu.ac.il>
2020-06-12 10:42:11 +0300

    Fix timer

--- Tool_code/OOP_project/Main_win.py
+++ Tool_code/OOP_project/Main_win.py
@@ -12,7 +12,7 @@ from SpeciesDB import *
 def timer(start,end):
     hours, rem = divmod(end-start, 3600)
     minutes, seconds = divmod(rem, 60)
-    print("{:0>2}:{:0>2}:{:05.2f}".format(int(hours),int(minutes),seconds))
+    return "{:0>2}:{:0>2}:{:05.2f}".format(int(hours), int(minutes), seconds)
 
 
 if __name__ == "__main__":
