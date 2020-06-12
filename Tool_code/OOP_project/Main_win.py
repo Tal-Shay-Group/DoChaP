7bc46576d5709cfec61bd9c4f3d4a16d933e2f3a
ShaniGalOz <galozs@post.bgu.ac.il>
2020-06-12 10:27:22 +0300

    Prepare to Run on all species

--- Tool_code/OOP_project/Main_win.py
+++ Tool_code/OOP_project/Main_win.py
@@ -8,11 +8,18 @@ from Director import Director
 from OrthologsBuilder import *
 from SpeciesDB import *
 
+
+def timer(start,end):
+    hours, rem = divmod(end-start, 3600)
+    minutes, seconds = divmod(rem, 60)
+    print("{:0>2}:{:0>2}:{:05.2f}".format(int(hours),int(minutes),seconds))
+
+
 if __name__ == "__main__":
     start_time = time.time()
     download = False
     withEns = True
-    species = ['M_musculus']#, 'H_sapiens', 'R_norvegicus', 'D_rerio', 'X_tropicalis']
+    species = ['M_musculus', 'H_sapiens', 'R_norvegicus', 'D_rerio', 'X_tropicalis']
 
     print("Running DBbuilder with Download {} and withENS {}".format(download, withEns))
 
@@ -21,7 +28,7 @@ if __name__ == "__main__":
     orthologs = OrthologsBuilder(species=species)
     director.setBuilder(orthologs)
     director.collectFromSource(download=download)
-    print("#### Orthologs collection duration: %s seconds" % (time.time()-bp))
+    print("#### Orthologs collection duration: " + timer(bp, time.time()))
 
     spl = len(species)
     spnum = 1
@@ -29,13 +36,13 @@ if __name__ == "__main__":
         print("===========Current Species: {}===========".format(sp))
         bp = time.time()
         dbBuild = dbBuilder(sp, download=download, withEns=withEns)
-        print("#### Species data collect & merge duration: %s seconds" % (time.time() - bp))
+        print("#### Species data collect & merge duration: " + timer(bp, time.time()))
         if spnum == 1:
             dbBuild.create_tables_db(merged=True)
         bp = time.time()
         dbBuild.fill_in_db(merged=True)
         print("Adding species {} to DB {} completed!".format(sp, dbBuild.dbName))
-        print("#### Duration: %s seconds" % (time.time() - bp))
+        print("#### Duration: " + timer(bp, time.time()))
         if spnum == spl:
             dbBuild.create_index()
             dbBuild.AddOrthology(orthologs.AllSpeciesDF)
@@ -45,5 +52,5 @@ if __name__ == "__main__":
         dbBuild.fill_in_db(merged=False)
         spnum += 1
         print("Filling {} completed!".format(dbBuild.dbName))
-        print("#### Duration: %s seconds" % (time.time() - bp))
-    print("#### Full run duration: %s minutes" % ((time.time() - start_time)/60))
+        print("#### Duration: " + timer(bp, time.time()))
+    print("#### Full run duration: " + timer(start_time, time.time()))
