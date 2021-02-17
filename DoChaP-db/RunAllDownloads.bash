a98dbe5f1ffeb414902f72b9a4b5abb8cc514a75
ShaniGalOz <galozs@post.bgu.ac.il>
2021-02-17 14:40:29 +0200

    Fix typo in RunAllDownloads.bash and add exit code if files not created properly

--- DoChaP-db/RunAllDownloads.bash
+++ DoChaP-db/RunAllDownloads.bash
@@ -3,7 +3,17 @@
 chmod -R 770 *
 
 # create the downloads shell scripts for ensembl domains and orthology from BioMart
-python CreateAlDownloadScripts.py
+python CreateAllDownloadScripts.py
+
+FILE=ShellScripts.txt
+if test -f "$FILE"; then
+    echo "$FILE exists."
+else
+    echo "$FILE not found"
+    exit 1
+fi
+
+chmod -R 770 *
 # Run the shell scripts
 while IFS='' read -r LINE || [ -n "$LINE" ]; do
         qsub -cwd -q tals.q -V ${LINE}
