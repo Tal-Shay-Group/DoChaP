39f425928c758c37863046f115d6243b4b958c48
Nimrod Haiat <nimnimch@gmail.com>
2021-02-20 12:39:01 +0200

    Move pdf creation to Display object

--- DoChaP-web/client/pages/results/resultsController.js
+++ DoChaP-web/client/pages/results/resultsController.js
@@ -209,54 +209,6 @@ angular.module("DoChaP")
 
         //downloading pdf
         $scope.downloadPDF = function () {
-            //init attributes
-            var doc = new jsPDF();
-            var space = 5;
-            var width = 90;
-            var height = 15;
-            var rowHeight = 40;
-            var startY = 40;
-            var transcriptsPerPage = 6;
-            doc.setFontSize(10);
-
-            //introduction text
-            doc.text(3 * space, 2 * space, self.geneInfo.gene_symbol + " " + self.geneInfo.specieName + ", " + +$scope.transcripts.length + " transcripts");
-            doc.text(3 * space, 3 * space, "chr" + self.geneInfo.chromosome + ":" + numberToTextWithCommas(self.geneInfo.scale.start) + "-" + numberToTextWithCommas(self.geneInfo.scale.end));
-
-            //drawing each transcript
-            for (var i = 0; i < $scope.transcripts.length; i++) {
-                if (i % transcriptsPerPage == 0) {
-                    //adding scale for every new page
-                    var canvasScaleGenomic = document.getElementById("canvas-scale");
-                    var imgGenomicScale = canvasScaleGenomic.toDataURL("image/png");
-                    var canvasScaleProtein = document.getElementById("canvas-scale-protein");
-                    var imgProteinScale = canvasScaleProtein.toDataURL("image/png");
-
-                    doc.addImage(imgGenomicScale, 3 * space, 3 * space, width, height * 1.5);
-                    doc.addImage(imgProteinScale, 3 * space + width + space, 3 * space, width, height * 1.5);
-                }
-                //getting images from canvases
-                var canvasGenomic = document.getElementById("canvas-genomic" + i);
-                var imgGenomic = canvasGenomic.toDataURL("image/png");
-                var canvasTranscript = document.getElementById("canvas-transcript" + i);
-                var imgTranscript = canvasTranscript.toDataURL("image/png");
-                var canvasProtein = document.getElementById("canvas-protein" + i);
-                var imgProtein = canvasProtein.toDataURL("image/png");
-                doc.setFontSize(10);
-                doc.text(3 * space, startY + rowHeight * (i % transcriptsPerPage), "Transcript: " + $scope.transcripts[i].name + " Protein: " + $scope.transcripts[i].protein_name);
-                //drawing. parameters:x,y,width,height
-                doc.addImage(imgGenomic, 3 * space, startY + space + rowHeight * (i % transcriptsPerPage), width, height);
-                doc.addImage(imgTranscript, 3 * space + width + space, startY + space + rowHeight * (i % transcriptsPerPage), width, height / 2);
-                doc.addImage(imgProtein, 3 * space + width + space, startY + space + rowHeight * (i % transcriptsPerPage) + height, width, height);
-
-                if ((i + 1) % transcriptsPerPage == 0) {
-                    //adding new page when finished full page
-                    doc.addPage();
-                }
-
-            }
-            //saving in user computer
-            doc.save(self.geneInfo.gene_symbol + ".pdf");
+            $scope.display.pdfCreator.create(self.geneInfo,"");
         }
-
     });
